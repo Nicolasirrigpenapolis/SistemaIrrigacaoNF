e988caf32114928e46e6107dc32beb503ab1b6cb
NicolasIrrigPenapolis <nicolasportie@irrigpenapolis.com.br>
2025-07-14 08:33:27 -0300

    Primeiro commit

---
+++ IRRIG/faz_exe.bat
@@ -0,0 +1,1 @@
+"c:\program files (x86)\microsoft visual studio\vb98\vb6.exe" c:\projetos\irrig\irrig\irrig.vbp /make C:\PROJETOS\IRRIG\IRRIG\IRRIG.exe
---
+++ IRRIG/faz_res.bat
@@ -0,0 +1,3 @@
+cd C:\Projetos\SistemaIrrigacao\figuras\
+"c:\program files (x86)\microsoft visual studio\vb98\wizards\rc.exe" -r -fo C:\Projetos\SistemaIrrigacao\IRRIG\IRRIG.res C:\Projetos\SistemaIrrigacao\IRRIG\IRRIG.rc
+cd C:\Projetos\SistemaIrrigacao\IRRIG\
